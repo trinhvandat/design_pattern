20283eaf82ed23796bfaec2e8b77a28eb5b713b1
leonard <trinhvandat90399@gmail.com>
2021-05-20 12:05:35 +0700

    chore: create demo for prototype pattern

---
+++ src/com/ptit/prototye/Square.java
@@ -0,0 +1,12 @@
+package com.ptit.prototye;
+
+public class Square extends Shape {
+    public Square() {
+        type = "Square";
+    }
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
+
